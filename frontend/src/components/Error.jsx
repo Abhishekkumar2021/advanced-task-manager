0a8c22a103a43e11a49de010447b4b6a36d97f40
Abhishekkumar2021 <abhishekku20022002@gmail.com>
2024-08-11 23:22:58 +0530

    add error page

--- frontend/src/components/Error.jsx
+++ frontend/src/components/Error.jsx
@@ -1,8 +1,12 @@
+import {Link} from 'react-router-dom'
 export default function Error() {
     return (
-        <div>
-            <h1>Error</h1>
-            <p>Page not found.</p>
+        <div className="w-full h-screen flex flex-col items-center justify-center bg-zinc-100">
+            <h1 className="text-9xl mb-3">
+                404 !
+            </h1>
+            <p className="text-4xl mb-12">Page not found.</p>
+            <Link to="/" className="text-2xl bg-zinc-900 text-white px-5 py-2 rounded-lg hover:bg-zinc-600 active:scale-95">Go back to home</Link>
         </div>
     );
 }
