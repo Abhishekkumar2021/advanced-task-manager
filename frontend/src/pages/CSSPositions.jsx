0df47bbc7c7f806095b38b9b288541e2c6214b15
Abhishekkumar2021 <abhishekku20022002@gmail.com>
2024-08-17 23:44:56 +0530

    understood CSS positioning

---
+++ frontend/src/pages/CSSPositions.jsx
@@ -0,0 +1,11 @@
+import '../styles/CSSPositions.css';
+
+export default function CSSPositions() {
+    return (
+        <div className="box">
+            <div className="parent">
+                <h1 className="child">Positioning Elements</h1>
+            </div>
+        </div>
+    )
+}
